9a0b9dd9cdd88e9d23ee5fa56fc9ee49a6a26e1b
Cahid <mr.cebizade@gmail.com>
2024-01-27 07:39:08 -0800

    9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/pyhton3
 def print_last_digit(number):
     last = number % 10 if number > 0 else number * -1 % 10
-    print(last, end='')
+    print('{}'.format(last), end='')
     return last
